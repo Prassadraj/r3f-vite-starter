375e3fd233ca599c810d9cf36ddace9b0bfae2e1
Prassad Raj R <prassadpr10@gmail.com>
2024-04-05 23:47:28 +0530

    chg

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,25 @@
-import { OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
+import { BoxGeometry } from "three";
+
 
 export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Falling", "Standing"],
+    },
+  });
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Sky />
+      <Environment preset="sunset" />
+      <group position-y={-1}>
+        <ContactShadows />
+        <Avatar animation={animation} />
+      </group>
     </>
   );
 };
